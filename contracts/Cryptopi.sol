69b5e0a2a23d72db7dc9157c1671a87e98980679
davidpurkiss <david.purkiss@skyscanner.net>
2021-10-08 19:48:02 +0100

    Updates to contracts

--- contracts/Cryptopi.sol
+++ contracts/Cryptopi.sol
@@ -3,15 +3,47 @@ pragma solidity ^0.8.0;
 import "./ERC721Tradable.sol";
 
 contract Cryptopi is ERC721Tradable {
-    constructor(address _proxyRegistryAddress)
-        ERC721Tradable("Cryptopi", "CPI", _proxyRegistryAddress)
-    {}
 
-    function baseTokenURI() override public pure returns (string memory) {
-        return "https://creatures-api.opensea.io/api/creature/";
+    /*
+     * Enforce the existence Cryptopi.
+     */
+    uint256 public constant MAX_SUPPLY;
+
+    /*
+    * Reserved for owners, giveaways, airdrops, etc.
+    */
+    uint public constant RESERVED_SUPPLY = 50;
+
+    string public baseTokenURI;
+    string public contractURI;
+    
+
+    constructor(
+        address _proxyRegistryAddress,
+        uint256 maxSupply,
+        string memory baseTokenUri,
+        string memory contractUri
+    )
+        ERC721Tradable("Cryptopi", "CPI", _proxyRegistryAddress)
+    {
+        MAX_SUPPLY = maxSupply;
+        baseTokenURI = baseTokenURI;
+        contractURI = contractURI;
     }
 
-    function contractURI() public pure returns (string memory) {
-        return "https://creatures-api.opensea.io/contract/opensea-creatures";
+    function baseTokenURI() override public view returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function contractURI() public view returns (string memory) {
+        return contractURI;
+    }
+
+    function maxSupply() public view returns (uint256) {
+        return MAX_SUPPLY;
+    }
+
+    function reservedSupply() public view returns (uint256) {
+        return RESERVED_SUPPLY;
     }
 }
--- contracts/CryptopiFactory.sol
+++ contracts/CryptopiFactory.sol
@@ -18,35 +18,27 @@ contract CreatureFactory is FactoryERC721, Ownable {
 
     address public proxyRegistryAddress;
     address public nftAddress;
-    address public lootBoxNftAddress;
     string public baseURI = "https://creatures-api.opensea.io/api/factory/";
 
     /*
-     * Enforce the existence of only 100 OpenSea creatures.
+     * Three different options for minting Cyptopi (1, 5, 15 20).
      */
-    uint256 CREATURE_SUPPLY = 100;
-
-    /*
-     * Three different options for minting Creatures (basic, premium, and gold).
-     */
-    uint256 NUM_OPTIONS = 3;
-    uint256 SINGLE_CREATURE_OPTION = 0;
-    uint256 MULTIPLE_CREATURE_OPTION = 1;
-    uint256 LOOTBOX_OPTION = 2;
-    uint256 NUM_CREATURES_IN_MULTIPLE_CREATURE_OPTION = 4;
+    uint256 NUM_OPTIONS = 5;
+    uint256 SINGLE_CRYPTOPI_OPTION = 0;
+    uint256 FIVE_CRYPTOPI_OPTION = 1;
+    uint256 TEN_CRYPTOPI_OPTION = 2;
+    uint256 FIFTEEN_CRYPTOPI_OPTION = 4;
+    uint256 TWENTY_CRYPTOPI_OPTION = 5;
 
     constructor(address _proxyRegistryAddress, address _nftAddress) {
         proxyRegistryAddress = _proxyRegistryAddress;
         nftAddress = _nftAddress;
-        lootBoxNftAddress = address(
-            new CreatureLootBox(_proxyRegistryAddress, address(this))
-        );
 
         fireTransferEvents(address(0), owner());
     }
 
     function name() override external pure returns (string memory) {
-        return "OpenSeaCreature Item Sale";
+        return "Cryptopi Item Sale";
     }
 
     function symbol() override external pure returns (string memory) {
@@ -73,6 +65,22 @@ contract CreatureFactory is FactoryERC721, Ownable {
         }
     }
 
+    function getTokensToMint(unint256 _optionId) private returns (uint256) {
+        if (_optionId == SINGLE_CRYPTOPI_OPTION) {
+            return 1;
+        } else if (_optionId == FIVE_CRYPTOPI_OPTION) {
+            return 5;
+        } else if (_optionId == TEN_CRYPTOPI_OPTION) {
+            return 10;
+        } else if (_optionId == FIFTEEN_CRYPTOPI_OPTION) {
+            return 15;
+        } else if (_optionId == TWENTY_CRYPTOPI_OPTION) {
+            return 20;
+        }
+
+        return 0;
+    }
+
     function mint(uint256 _optionId, address _toAddress) override public {
         // Must be sent from the owner proxy or owner.
         ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
@@ -83,22 +91,16 @@ contract CreatureFactory is FactoryERC721, Ownable {
         );
         require(canMint(_optionId));
 
-        Creature openSeaCreature = Creature(nftAddress);
-        if (_optionId == SINGLE_CREATURE_OPTION) {
-            openSeaCreature.mintTo(_toAddress);
-        } else if (_optionId == MULTIPLE_CREATURE_OPTION) {
-            for (
-                uint256 i = 0;
-                i < NUM_CREATURES_IN_MULTIPLE_CREATURE_OPTION;
-                i++
-            ) {
-                openSeaCreature.mintTo(_toAddress);
-            }
-        } else if (_optionId == LOOTBOX_OPTION) {
-            CreatureLootBox openSeaCreatureLootBox = CreatureLootBox(
-                lootBoxNftAddress
-            );
-            openSeaCreatureLootBox.mintTo(_toAddress);
+        Crypotpi cryptopi = Crypotpi(nftAddress);
+
+        uint256 tokensToMint = getTokensToMint(_optionId);
+
+        for (
+            uint256 i = 0;
+            i < tokensToMint;
+            i++
+        ) {
+            cryptopi.mintTo(_toAddress);
         }
     }
 
@@ -107,21 +109,13 @@ contract CreatureFactory is FactoryERC721, Ownable {
             return false;
         }
 
-        Creature openSeaCreature = Creature(nftAddress);
-        uint256 creatureSupply = openSeaCreature.totalSupply();
+        Cryptopi cryptopi = Cryptopi(nftAddress);
+        uint256 cryptopiSupply = cryptopi.totalSupply();
+        uint256 cryptopiMaxSupply = cryptopi.maxSupply();
 
-        uint256 numItemsAllocated = 0;
-        if (_optionId == SINGLE_CREATURE_OPTION) {
-            numItemsAllocated = 1;
-        } else if (_optionId == MULTIPLE_CREATURE_OPTION) {
-            numItemsAllocated = NUM_CREATURES_IN_MULTIPLE_CREATURE_OPTION;
-        } else if (_optionId == LOOTBOX_OPTION) {
-            CreatureLootBox openSeaCreatureLootBox = CreatureLootBox(
-                lootBoxNftAddress
-            );
-            numItemsAllocated = openSeaCreatureLootBox.itemsPerLootbox();
-        }
-        return creatureSupply < (CREATURE_SUPPLY - numItemsAllocated);
+        uint256 numItemsAllocated = getTokensToMint(_optionId);
+        
+        return cryptopi < (cryptopiMaxSupply - numItemsAllocated);
     }
 
     function tokenURI(uint256 _optionId) override external view returns (string memory) {
